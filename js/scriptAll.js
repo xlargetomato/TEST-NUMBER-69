7faa2295953eda43193e1573dd0beb03f209894b
Ahmed Hegazy <149302898+xlargetomato@users.noreply.github.com>
2024-01-25 20:26:38 +0200

    Add files via upload

--- js/scriptAll.js
+++ js/scriptAll.js
@@ -1,21 +1,22 @@
 const LINKS = document.querySelectorAll(".link");
-//// Index >> mainmenu = (0),lecture = (1),assignments = (2)||,controlpanel = (3),,about = (5),
+//// Index >> mainmenu = (0),course = (1),leacture = (2)||,assign = (3),,controlpanel = (4),about = (6),
 ////////////////////////////////////
-
-const lectureList = document.querySelector(".slidedown");
+// const lectureList = document.querySelector(".slidedown");
 const listA = document.querySelector(".listA");
-lectureList.onclick = () => {
+const listchilds = document.querySelectorAll(".listA li");
+listchilds[0].addEventListener("click", function () {
+  window.location = "lectureOrquestions.html";
+});
+LINKS[2].onclick = () => {
   listA.classList.toggle("BLOCK");
-  lectureList.classList.toggle("fa-caret-down");
-  lectureList.classList.toggle("fa-caret-up");
 };
 LINKS[0].addEventListener("click", () => {
   window.location = "index.html";
 });
 LINKS[1].addEventListener("click", () => {
-  window.location = "lecture.html";
+  window.location = "course.html";
 });
-LINKS[2].addEventListener("click", () => {
+LINKS[3].addEventListener("click", () => {
   window.location = "assignments.html";
 });
 // LINKS[99].addEventListener("click", () => {
