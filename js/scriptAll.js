d3dc6cc9733bdf94e6a8046837beb12e2854273e
Ahmed Hegazy <149302898+xlargetomato@users.noreply.github.com>
2024-02-03 23:20:50 +0200

    Add files via upload

--- js/scriptAll.js
+++ js/scriptAll.js
@@ -7,12 +7,16 @@ const listchilds = document.querySelectorAll(".listA li");
 const HUM = document.querySelector(".HUM");
 const sidebar = document.querySelector(".sidebar");
 const page = document.querySelector(".page");
+const anihover = document.querySelectorAll(".ani-hover");
 
 listchilds[0].addEventListener("click", function () {
   window.location = "lectureOrquestions.html";
 });
 LINKS[2].onclick = () => {
   listA.classList.toggle("BLOCK");
+  setTimeout(() => {
+    listA.classList.toggle("translate");
+  });
 };
 LINKS[0].addEventListener("click", () => {
   window.location = "index.html";
@@ -23,13 +27,16 @@ LINKS[1].addEventListener("click", () => {
 LINKS[3].addEventListener("click", () => {
   window.location = "assignments.html";
 });
+LINKS[4].addEventListener("click", () => {
+  window.location = "controlpanel.html";
+});
 HUM.addEventListener("click", () => {
   sidebar.classList.toggle("BLOCK");
   HUM.classList.toggle("move-right");
-  page.classList.toggle("opacity");
+  page.classList.toggle("opacity-color");
   setTimeout(() => {
     sidebar.classList.toggle("translate");
-  }, "5");
+  });
 });
 // LINKS[99].addEventListener("click", () => {
 //   window.location = "index.html";
